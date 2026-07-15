a54860b0fcf698187072545a3230a930026bfee8
alfario207 <alfariolilhuda@gmail.com>
2026-07-15 22:25:36 +0700

    create ui

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import "./styles.css"
+
+const navbar = document.querySelectorAll('.project')
+
+navbar.forEach(button => {
+    button.addEventListener('click', (e) => {
+        const id = e.currentTarget.id
+        console.log('btn clicked id=',id)
+
+        navbar.forEach(nav => nav.classList.remove('selected'))
+        
+        e.currentTarget.classList.add('selected');
+    })
+})
